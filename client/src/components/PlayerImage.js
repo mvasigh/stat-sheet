1ccd5d4101820ad8bc3392ae6bb606ba88409482
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-10 23:50:47 -0500

    add alt prop to PlayerImage component

--- client/src/components/PlayerImage.js
+++ client/src/components/PlayerImage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PlayerImage = ({ src }) => {
+const PlayerImage = ({ src, alt }) => {
   return (
     <div className="player-image">
-      <img src={src} alt="" />
+      <img src={src} alt={alt} />
     </div>
   );
 };
 
 PlayerImage.propTypes = {
-  src: PropTypes.string
+  src: PropTypes.string,
+  alt: PropTypes.string
 };
 
 export default PlayerImage;
