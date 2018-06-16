1fde353f7219cfd0a9dcb5b12db4f27cdcc67f99
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-15 21:07:15 -0500

    add team colors data

---
+++ client/src/data/index.js
@@ -0,0 +1,276 @@
+const data = {
+  teams: [
+    {
+      abbr: 'ATL',
+      city: 'Atlanta',
+      name: 'Hawks',
+      colors: {
+        primary: '#E13A3E',
+        secondary: '#c4d600'
+      }
+    },
+    {
+      abbr: 'BOS',
+      city: 'Boston',
+      name: 'Celtics',
+      colors: {
+        primary: '#008348',
+        secondary: '#bb9753'
+      }
+    },
+    {
+      abbr: 'BKN',
+      city: 'Brooklyn',
+      name: 'Nets',
+      colors: {
+        primary: '#061922',
+        secondary: '#FFFFFF'
+      }
+    },
+    {
+      abbr: 'CHA',
+      city: 'Charlotte',
+      name: 'Hornets',
+      colors: {
+        primary: '#1D1160',
+        secondary: '#008CA8'
+      }
+    },
+    {
+      abbr: 'CHI',
+      city: 'Chicago',
+      name: 'Bulls',
+      colors: {
+        primary: '#CE1141',
+        secondary: '#061922'
+      }
+    },
+    {
+      abbr: 'CLE',
+      city: 'Cleveland',
+      name: 'Cavaliers',
+      colors: {
+        primary: '#860038',
+        secondary: '#002D62'
+      }
+    },
+    {
+      abbr: 'DAL',
+      city: 'Dallas',
+      name: 'Mavericks',
+      colors: {
+        primary: '#00538C',
+        secondary: '#002B5E'
+      }
+    },
+    {
+      abbr: 'DEN',
+      city: 'Denver',
+      name: 'Nuggets',
+      colors: {
+        primary: '#0C2340',
+        secondary: '#FDB927'
+      }
+    },
+    {
+      abbr: 'DET',
+      city: 'Detroit',
+      name: 'Pistons',
+      colors: {
+        primary: '#ED174C',
+        secondary: '#006BB6'
+      }
+    },
+    {
+      abbr: 'GSW',
+      city: 'Golden State',
+      name: 'Warriors',
+      colors: {
+        primary: '#006BB6',
+        secondary: '#FDB927'
+      }
+    },
+    {
+      abbr: 'HOU',
+      city: 'Houston',
+      name: 'Rockets',
+      colors: {
+        primary: '#CE1141',
+        secondary: '#C4CED4'
+      }
+    },
+    {
+      abbr: 'IND',
+      city: 'Indiana',
+      name: 'Pacers',
+      colors: {
+        primary: '#002D62',
+        secondary: '#FDBB30'
+      }
+    },
+    {
+      abbr: 'LAC',
+      city: 'Los Angeles',
+      name: 'Clippers',
+      colors: {
+        primary: '#ED174C',
+        secondary: '#006BB6'
+      }
+    },
+    {
+      abbr: 'LAL',
+      city: 'Los Angeles',
+      name: 'Lakers',
+      colors: {
+        primary: '#552583',
+        secondary: '#FDB927'
+      }
+    },
+    {
+      abbr: 'MEM',
+      city: 'Memphis',
+      name: 'Grizzlies',
+      colors: {
+        primary: '#00285E',
+        secondary: '#6189B9'
+      }
+    },
+    {
+      abbr: 'MIA',
+      city: 'Miami',
+      name: 'Heat',
+      colors: {
+        primary: '#98002E',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'MIL',
+      city: 'Milwaukee',
+      name: 'Bucks',
+      colors: {
+        primary: '#00471B',
+        secondary: '#EEE1C6'
+      }
+    },
+    {
+      abbr: 'MIN',
+      city: 'Minnesota',
+      name: 'Timberwolves',
+      colors: {
+        primary: '#0C2340',
+        secondary: '#78BE20'
+      }
+    },
+    {
+      abbr: 'NOP',
+      city: 'New Orleans',
+      name: 'Pelicans',
+      colors: {
+        primary: '#0C2340',
+        secondary: '#85714D'
+      }
+    },
+    {
+      abbr: 'NYK',
+      city: 'New York',
+      name: 'Knicks',
+      colors: {
+        primary: '#006BB6',
+        secondary: '#F58426'
+      }
+    },
+    {
+      abbr: 'OKC',
+      city: 'Oklahoma City',
+      name: 'Thunder',
+      colors: {
+        primary: '#002D62',
+        secondary: '#EF3B24'
+      }
+    },
+    {
+      abbr: 'ORL',
+      city: 'Orlando',
+      name: 'Magic',
+      colors: {
+        primary: '#0077C0',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'PHI',
+      city: 'Philadelphia',
+      name: '76ers',
+      colors: {
+        primary: '#006BB6',
+        secondary: '#ED174C'
+      }
+    },
+    {
+      abbr: 'PHO',
+      city: 'Phoenix',
+      name: 'Suns',
+      colors: {
+        primary: '#E56020',
+        secondary: '#1D1160'
+      }
+    },
+    {
+      abbr: 'POR',
+      city: 'Portland',
+      name: 'Trail Blazers',
+      colors: {
+        primary: '#E03A3E',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'SAC',
+      city: 'Sacramento',
+      name: 'Kings',
+      colors: {
+        primary: '#5A2D81',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'SAS',
+      city: 'San Antonio',
+      name: 'Spurs',
+      colors: {
+        primary: '#C4CED4',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'TOR',
+      city: 'Toronto',
+      name: 'Raptors',
+      colors: {
+        primary: '#CE1141',
+        secondary: '#000000'
+      }
+    },
+    {
+      abbr: 'UTA',
+      city: 'Utah',
+      name: 'Jazz',
+      colors: {
+        primary: '#002B5C',
+        secondary: '#F9A01B'
+      }
+    },
+    {
+      abbr: 'WAS',
+      city: 'Washington',
+      name: 'Wizards',
+      colors: {
+        primary: '#002B5C',
+        secondary: '#E31837'
+      }
+    }
+  ]
+};
+
+export default data;
