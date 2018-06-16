e3cc85377ec5574419010589a66457e7c27890cf
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-15 21:05:58 -0500

    list player stats in <ul>

--- client/src/components/PlayerStats.js
+++ client/src/components/PlayerStats.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const PlayerStats = ({ stats }) => {
-  return <div>{JSON.stringify(stats)}</div>;
+  const statsList = Object.entries(stats).map((stat, i) => {
+    const [category, value] = stat;
+    return (
+      <li key={i}>
+        {category}: {value}
+      </li>
+    );
+  });
+  return <ul className="player-stats">{statsList}</ul>;
 };
 
 export default PlayerStats;
