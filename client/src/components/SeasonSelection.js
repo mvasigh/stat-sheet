903d0bbaf0c62970d693fb2b857dcea2ce15a02a
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-18 20:23:46 -0500

    add custom styling to SeasonSelection component

--- client/src/components/SeasonSelection.js
+++ client/src/components/SeasonSelection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { all } from '../config/seasons';
 
+import './SeasonSelection.css';
+
 const SeasonSelection = ({
   allSeasons = all,
   selectedSeason = '',
@@ -11,7 +13,11 @@ const SeasonSelection = ({
     onSeasonChange(_season.value);
   };
   return (
-    <select ref={select => (_season = select)} onChange={handleChange}>
+    <select
+      className="season-selection"
+      ref={select => (_season = select)}
+      onChange={handleChange}
+    >
       {allSeasons.map((season, i) => (
         <option key={i} value={season.name}>
           {season.label}
