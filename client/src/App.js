d3f687a53850be1590c556444379b026f475584c
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-18 20:24:55 -0500

    populate playersList on componentDidMount and on season change

--- client/src/App.js
+++ client/src/App.js
@@ -9,27 +9,45 @@ class App extends Component {
     super(props);
     this.state = {
       selectedSeason: '2018-playoff',
+      playersList: [],
       activeSlot: 1,
       players: Array(2)
     };
     this.handleSeasonChange = this.handleSeasonChange.bind(this);
     this.handlePlayerSearch = this.handlePlayerSearch.bind(this);
   }
-  handleSeasonChange(season) {
-    this.setState({ selectedSeason: season });
+
+  componentDidMount() {
+    this.getPlayersList(this.state.selectedSeason).then(playersList =>
+      this.setState({ playersList })
+    );
   }
+
+  async handleSeasonChange(season) {
+    const playersList = await this.getPlayersList(season);
+    this.setState({ selectedSeason: season, playersList });
+  }
+
   async handlePlayerSearch(playerName) {
     let { players, activeSlot } = this.state;
     players[activeSlot - 1] = await this.getPlayer(playerName);
     activeSlot = activeSlot === 1 ? 2 : 1;
     this.setState({ players, activeSlot });
   }
+
   getPlayer(player, season = this.state.selectedSeason) {
     return fetch(`/api/season/${season}/player/${player}`)
       .then(response => response.json())
       .then(player => ({ ...player, season }))
       .catch(e => console.log(e));
   }
+
+  getPlayersList(season = this.state.selectedSeason) {
+    return fetch(`/api/season/${season}/player/`)
+      .then(response => response.json())
+      .catch(e => console.log(e));
+  }
+
   render() {
     return (
       <div className="app">
