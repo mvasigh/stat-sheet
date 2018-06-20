25cda63717ffc0e2471ed922ecb1908644e71ca3
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-20 08:14:27 -0500

    render StatCategoryList and add player stats comparison when both player slots are filled

--- client/src/components/PlayerComparison.js
+++ client/src/components/PlayerComparison.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PlayerView from './PlayerView';
+import StatCategoryList from './StatCategoryList';
 
 import './PlayerComparison.css';
 
-const PlayerComparison = ({ players = [] }) => {
+const PlayerComparison = ({ players }) => {
+  let comparison;
+  if (players[0] && players[1]) {
+    comparison = Object.keys(players[0].stats).map(stat => ({
+      label: stat,
+      comparison: Number(players[0].stats[stat]) - Number(players[1].stats[stat])
+    }));
+  }
   return (
     <div className="player-comparison">
-      {players.map((player, i) => <PlayerView player={player} key={i + 1} />)}
+      <div className="player-comparison__container">
+        <PlayerView player={players[0]} />
+        <StatCategoryList comparison={comparison} />
+        <PlayerView player={players[1]} />
+      </div>
     </div>
   );
 };
