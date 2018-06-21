ae0178f2ebb1447e3cae5a5aa3aa674f34a7a227
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-21 12:14:05 -0500

    add seasons to data file and adjust dummy data

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,4 +1,12 @@
 const data = {
+  seasons: [
+    { name: '2018-playoff', label: '2018 Playoffs' },
+    { name: '2017-2018-regular', label: '2017-2018 Regular Season' },
+    { name: '2017-playoff', label: '2017 Playoffs' },
+    { name: '2016-2017-regular', label: '2016-2017 Regular Season' },
+    { name: '2016-playoff', label: '2016 Playoffs' },
+    { name: '2015-2016-regular', label: '2015-2016 Regular Season' }
+  ],
   teams: [
     {
       abbr: 'ATL',
@@ -273,7 +281,7 @@ const data = {
     {
       abbr: 'DUM',
       city: 'Placeholder',
-      name: 'Dummy',
+      name: ' ',
       colors: {
         primary: '#111',
         secondary: '#222'
@@ -282,18 +290,26 @@ const data = {
   ],
   dummyPlayer: {
     info: {
-      FirstName: '',
-      LastName: '',
-      JerseyNumber: '',
+      FirstName: 'Search for',
+      LastName: 'a player',
+      JerseyNumber: ' ',
       Position: '',
       Height: '',
       Weight: '',
       ImageSrc: '',
       team: {
-        Name: 'Dummy'
+        City: '',
+        Name: ' '
       }
     },
-    stats: {},
+    stats: {
+      1: '-',
+      2: '-',
+      3: '-',
+      4: '-',
+      5: '-',
+      6: '-'
+    },
     season: {}
   },
   categories: [
