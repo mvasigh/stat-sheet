10fe451fcd075a87aa9a79f478d54712fa51467e
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-21 07:33:58 -0500

    conditionally render red and green arrows based on stat comparison

--- client/src/components/StatCategory.js
+++ client/src/components/StatCategory.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import UpArrowIcon from './UpArrowIcon';
+import DownArrowIcon from './DownArrowIcon';
+
+import './StatCategory.css';
 
 const StatCategory = ({ label, comparison }) => {
   return (
     <li className="stat-category">
-      {label} - {comparison}
+      {comparison === 0 ? (
+        ''
+      ) : comparison > 0 ? (
+        <UpArrowIcon />
+      ) : (
+        <DownArrowIcon />
+      )}
+      <div className="stat-category__label">{label}</div>
+      {comparison === 0 ? (
+        ''
+      ) : comparison > 0 ? (
+        <DownArrowIcon />
+      ) : (
+        <UpArrowIcon />
+      )}
     </li>
   );
 };
