fef5804dcac7a8f16947f8e8b4c263a007b950e7
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-24 20:46:33 -0500

    add isActive prop to PlayerViews and handle player select

--- client/src/components/PlayerComparison.js
+++ client/src/components/PlayerComparison.js
@@ -4,20 +4,31 @@ import StatCategoryList from './StatCategoryList';
 
 import './PlayerComparison.css';
 
-const PlayerComparison = ({ players }) => {
+const PlayerComparison = ({ players, activeSlot, onPlayerSelect = f => f }) => {
   let comparison;
   if (players[0] && players[1]) {
     comparison = Object.keys(players[0].stats).map(stat => ({
       label: stat,
-      comparison: Number(players[0].stats[stat]) - Number(players[1].stats[stat])
+      comparison:
+        Number(players[0].stats[stat]) - Number(players[1].stats[stat])
     }));
   }
   return (
     <div className="player-comparison">
       <div className="player-comparison__container">
-        <PlayerView player={players[0]} />
+        <PlayerView
+          player={players[0]}
+          isActive={activeSlot == 1}
+          slotNumber={1}
+          onPlayerSelect={onPlayerSelect}
+        />
         <StatCategoryList comparison={comparison} />
-        <PlayerView player={players[1]} />
+        <PlayerView
+          player={players[1]}
+          isActive={activeSlot == 2}
+          slotNumber={2}
+          onPlayerSelect={onPlayerSelect}
+        />
       </div>
     </div>
   );
